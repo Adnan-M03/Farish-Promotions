c71e06aef411bb81f1b3748d25d6d95c75e94d84
DevByAdnan <adufh5073@gmail.com>
2025-07-06 11:40:39 +0300

    netlify 2

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: './',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  },
   server: {
     port: 5173,
     open: true
