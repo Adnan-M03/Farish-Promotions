c9c22c643204b8169ba0bf0fe88fba11cce6938d
DevByAdnan <adufh5073@gmail.com>
2025-07-10 17:01:24 +0300

    Added a preloader animation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,23 +7,43 @@ import WhySection from "./WhySection";
 import Contact from "./Contact";
 import Minfooter from "./Minfooter";
 
-
+import { useState, useEffect } from "react";
 
 import "./css/index.css"
 
 function App() {
+    const [isLoading, setIsLoading] = useState(true);
+    const handleLoad = () => {
+        setIsLoading(false);
+        console.log(isLoading);
+    }
+    useEffect(() => {
+        window.addEventListener("load", handleLoad);
+        return () => {
+            window.removeEventListener("load", handleLoad);
+        }
+    }, [])
+
+
     return (
         <>
-            <Header/>
-            <Main>
-                <Service/>
-                <About/>
-                <WhySection/>
-            </Main>
-            <Footer>
-                <Contact/>
-                <Minfooter/>
-            </Footer>
+            <div className="appContainer" style={{
+                display: isLoading ? "hidden" : "visible"
+            }}>
+                <Header />
+                <Main>
+                    <Service />
+                    <About />
+                    <WhySection />
+                </Main>
+                <Footer>
+                    <Contact />
+                    <Minfooter />
+                </Footer>
+            </div>
+            <div className="loader" style={{
+                visibility: isLoading ? "visible" : "hidden"
+            }} ></div>
         </>
     );
 }
